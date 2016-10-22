f3a2a6a80f6112cc11b3c8fef3a0f2b999ff0410
KMH690 <22khoward@davis.k12.ut.us>
2016-10-22 11:37:22 -0600

    Create ticTac.js

---
+++ ticTac.js
@@ -0,0 +1,24 @@
+var turn = 'X';
+var score = {
+    'X': 0,
+    'O': 0
+};
+var gridValue = 0;
+function fnLoad() {
+    var select = document.getElementById("grid");
+    for (i = 3; i <= 100; i += 1) {
+        var option = document.createElement('option');
+        select.options[select.options.length] = new Option(i + ' X ' + i, i);
+    }
+    addEvent(document.getElementById("game"), "click", fnChoose);
+
+    fnNewGame();
+}
+function addEvent(element, eventName, callback) {
+
+    if (element.addEventListener) {
+        element.addEventListener(eventName, callback, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + eventName, callback);
+    }
+}
