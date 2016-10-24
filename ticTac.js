8fe4d3635fa9bf59581cb227512eede65eb8d64b
KMH690 <22khoward@davis.k12.ut.us>
2016-10-24 15:21:51 -0600

    Update ticTac.js

--- ticTac.js
+++ ticTac.js
@@ -22,3 +22,46 @@ function addEvent(element, eventName, callback) {
         element.attachEvent("on" + eventName, callback);
     }
 }
+function fnChoose(e) {
+    if (e.target && e.target.nodeName == "TD") {
+        var targetElement = document.getElementById(e.target.id);
+        var prevTurn;
+        if ((targetElement.className).indexOf("disabled") == -1) {
+            targetElement.innerHTML = turn;
+            targetElement.classList.add('disabled');
+            targetElement.classList.add(turn);
+            score[turn] += 1;
+            prevTurn = turn;
+            turn = turn === "X" ? "O" : "X";
+            if (fndecide(targetElement, prevTurn)) {
+                alert(prevTurn + ' has won the game');
+                fnNewGame();
+            } else if ((score['X'] + score['O']) == (gridValue * gridValue)) {
+                alert('Draw!');
+                fnNewGame();
+            }
+        }
+    }
+}
+function fndecide(targetElement, prevTurn) {
+    var UL = document.getElementById('game');
+    var elements, i, j, cnt;
+    if (score[prevTurn] >= gridValue) {
+        var classes = targetElement.className.split(/\s+/);
+        for (i = 0; i < classes.length; i += 1) {
+            cnt = 0;
+            if (classes[i].indexOf('row') !== -1 || classes[i].indexOf('col') !== -1 || classes[i].indexOf('dia') !== -1) {
+                elements = UL.getElementsByClassName(classes[i]);
+                for (j = 0; j < elements.length; j += 1) {
+                    if (elements[j].innerHTML == prevTurn) {
+                        cnt += 1;
+                    }
+                }
+                if (cnt == gridValue) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
